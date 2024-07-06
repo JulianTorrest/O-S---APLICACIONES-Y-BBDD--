8513cddd24bd814c04079fb76aae1dab95d35fd6
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-07-05 21:31:07 -0500

    Update Center.cs

--- Models/Center.cs
+++ Models/Center.cs
@@ -1,1 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerAccessControl.Models
+{
+    public class Center
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
 
