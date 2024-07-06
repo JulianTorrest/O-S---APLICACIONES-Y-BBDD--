25b28fbcbc54e52780a43c369c9b344a19c4cab4
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-07-05 21:31:23 -0500

    Update Entry.cs

--- Models/Entry.cs
+++ Models/Entry.cs
@@ -1,1 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerAccessControl.Models
+{
+    public class Entry
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int CenterId { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
 
