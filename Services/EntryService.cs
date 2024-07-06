9910f621e346f60cdcbe43fb5799dbc436a3ff6d
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-07-05 21:30:37 -0500

    Update EntryService.cs

--- Services/EntryService.cs
+++ Services/EntryService.cs
@@ -1,1 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using CustomerAccessControl.Data;
+using CustomerAccessControl.Models;
+
+namespace CustomerAccessControl.Services
+{
+    public class EntryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void RegisterEntry(Entry entry)
+        {
+            _context.Entries.Add(entry);
+            _context.SaveChanges();
+        }
+
+        // Additional methods for getting entries can be implemented here
+    }
+}
 
