98bedd2b945715ff11555773fef25d2008e316f6
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-07-05 21:20:48 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,1 +1,7 @@
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<CustomerEntryContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+    services.AddControllers();
+}
 
