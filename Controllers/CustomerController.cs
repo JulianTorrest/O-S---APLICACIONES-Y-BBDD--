8dc337437d32bdd1bf7f0e3f0cacc75864e22731
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-07-05 21:20:20 -0500

    Update CustomerController.cs

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,1 +1,32 @@
+// Controllers/CustomersController.cs
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    private readonly CustomerEntryContext _context;
+
+    public CustomersController(CustomerEntryContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Customer>> RegisterCustomer(Customer customer)
+    {
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerID }, customer);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Customer>> GetCustomer(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        return customer;
+    }
+}
 
